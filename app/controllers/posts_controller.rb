999c7d4cf592a81074e5836c7781f0d04436fc6d
ugajin <t.ugajin0804@gmail.com>
2016-02-11 23:49:29 +0900

    大体の機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,7 +20,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.html { redirect_to root_path }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new }
@@ -32,7 +32,7 @@ class PostsController < ApplicationController
   def update
     respond_to do |format|
       if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
+        format.html { redirect_to root_path }
         format.json { render :show, status: :ok, location: @post }
       else
         format.html { render :edit }
