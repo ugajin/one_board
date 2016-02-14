8947f5c38f40be3c16b071684973d713208e7437
ugajin <t.ugajin0804@gmail.com>
2016-02-14 17:27:42 +0900

    seed追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+
+user1 = User.create(name: "hoge", email: "hoge@mail.com", password: "hogehoge")
+user2 = User.create(name: "fuga", email: "fuga@mail.com", password: "fugafuga")
+user3 = User.create(name: "moge", email: "moge@mail.com", password: "mogemoge")
+
+contents = ["きのこ派です", "たけのこ派です", "争いはよくない"]
+images = ["kinoko.jpg", "takenoko.jpg", nil]
+
+15.times do |no|
+  num = rand(3)
+  if num == 2
+    Post.create(content: contents[num], user_id: num+1)
+  else
+    Post.create(content: contents[num], image: open("#{Rails.root}/db/data/#{images[num]}"), user_id: num+1)
+  end
+end
